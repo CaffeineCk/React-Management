cc49382aebf75de7c37a44f6178c1db714358d58
CaffeineCk <caffeinecookies@gmail.com>
2020-05-13 21:46:42 +0900

    Create CreateCustmoer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
 
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '홍길동',
+  'birthday' : '961222',
+  'gender' : '남',
+  'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '김현식',
+  'birthday' : '961322',
+  'gender' : '남',
+  'job' : '대학생'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '김숙식',
+  'birthday' : '961232',
+  'gender' : '여',
+  'job' : '프로그래머'
+}
+]
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>system</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer 
+              key = {c.id}
+              id = {c.id} 
+              image = {c.image} 
+              name={c.name} 
+              birthday={c.birthday} 
+              gender={c.gender} 
+              job = {c.job} >        
+            </Customer>
+          )
+        })
+      }
+      
     </div>
   );
 }
